93ef583667555578815535ca1123a149c3d41221
Carlos-Henrique98 <ichfs9108@gmail.com>
2021-10-02 13:14:02 -0300

    listar e buscar por id

---
+++ forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -0,0 +1,72 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.model.Curso
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.model.Usuario
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class TopicoService(private var topicos: List<Topico>) {
+
+    init {
+        val topico = Topico(
+            id = 1,
+            titulo = "Dúvida Kotlin",
+            mensagem = "Varíaveis no kotlin",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana da Silva",
+                email = "ana@email.com"
+            )
+        )
+
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Dúvida Kotlin 2",
+            mensagem = "Varíaveis no kotlin",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana da Silva",
+                email = "ana@email.com"
+            )
+        )
+
+        val topico3 = Topico(
+            id = 3,
+            titulo = "Dúvida Kotlin 3",
+            mensagem = "Varíaveis no kotlin",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana da Silva",
+                email = "ana@email.com"
+            )
+        )
+        topicos = Arrays.asList(topico, topico, topico)
+    }
+
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): Topico {
+        return topicos.stream().filter({ t ->
+            t.id == id
+        }).findFirst().get()
+    }
+}
